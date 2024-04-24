c904c3c8f6c7e523f3bbbe9babc721bb0932a8c5
awinds <awinds@lijian.net>
2024-04-24 21:31:19 +0800

    修正分页的问题

--- core/pager.php
+++ core/pager.php
@@ -1,13 +1,13 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <!-- 分页 -->
-<nav id="pager" class="flex items-center">
-    <?php $this->pageNav("&lt;上一页", "下一页&gt;", 1, "...", [
-        'wrapTag'=>"ul",
-        'wrapClass'=>'xa-pager mx-auto w-full flex justify-center space-x-4 mx-4 lg:justify-start lg:pl-32 lg:mr-32',
-        'itemTag'=>'li',
-        'textTag'=>'',
-        'itemClass'=>'',
-        'currentClass'=>'xa-pager-selected',
-        'prevClass'=>'xa-pn',
-        'nextClass'=>'xa-pn']); ?>
+<nav id="pager" class="flex items-center mx-auto">
+    <?php $this->pageNav("&lt;上一页", "下一页&gt;", 1, "...", array(
+    "wrapTag"=>"ul",
+    "wrapClass"=>"xa-pager mx-auto w-full flex justify-center space-x-4 mx-4 lg:justify-start lg:pl-32 lg:mr-32",
+    "itemTag"=>"li",
+    "textTag"=>"",
+    "itemClass"=>"",
+    "currentClass"=>"xa-pager-selected",
+    "prevClass"=>"xa-pn",
+    "nextClass"=>"xa-pn")); ?>
 </nav>
