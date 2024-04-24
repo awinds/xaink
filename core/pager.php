06bed40689a4e3a450dc3c30657a489bb6afde0d
awinds <awinds@lijian.net>
2024-04-24 21:12:24 +0800

    分页代码上更完整

--- core/pager.php
+++ core/pager.php
@@ -2,12 +2,12 @@
 <!-- 分页 -->
 <nav id="pager" class="flex items-center">
     <?php $this->pageNav("&lt;上一页", "下一页&gt;", 1, "...", [
-        wrapTag=>"ul",
-        wrapClass=>"xa-pager mx-auto w-full flex justify-center space-x-4 mx-4 lg:justify-start lg:pl-32 lg:mr-32",
-        itemTag=>"li",
-        textTag=>"",
-        itemClass=>"",
-        currentClass=>"xa-pager-selected",
-        prevClass=>"xa-pn",
-        nextClass=>"xa-pn",]); ?>
+        'wrapTag'=>"ul",
+        'wrapClass'=>'xa-pager mx-auto w-full flex justify-center space-x-4 mx-4 lg:justify-start lg:pl-32 lg:mr-32',
+        'itemTag'=>'li',
+        'textTag'=>'',
+        'itemClass'=>'',
+        'currentClass'=>'xa-pager-selected',
+        'prevClass'=>'xa-pn',
+        'nextClass'=>'xa-pn']); ?>
 </nav>
